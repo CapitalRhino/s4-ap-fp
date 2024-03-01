60d9c6e2427f2b977fb7b2b2702fd7c126e66031
Kaloyan Stoykov <kaloian66@gmail.com>
2024-03-01 19:55:06 +0100

    Week2: Unit tests done and added comments where needed

--- wk2/src/Pythagoras2.elm
+++ wk2/src/Pythagoras2.elm
@@ -50,13 +50,76 @@ arePythTriplesFilter: List (Int, Int, Int) -> List (Int, Int, Int)
 arePythTriplesFilter tripleList =
     List.filter isTripleTuple tripleList
 
+
+
 arePythTriplesRec: List (Int, Int, Int) -> List (Int, Int, Int)
 arePythTriplesRec tripleList = 
     case tripleList of 
+        -- Base Case
         [] ->
             []
+        
         x :: xs ->
+            -- Check if first element (x) is a triple tuple and call recursively for the rest of the list -> (x + y) :: arePythTriplesRec(rest of tuples list)
             if isTripleTuple x then
                 List.singleton x ++ arePythTriplesRec xs
+
+            -- Else - means x was not a triple tuple - call recursively for the rest of the list as parameter.
             else
                 arePythTriplesRec xs
+
+
+testPythTriplesMap: Bool
+testPythTriplesMap = (pythTriplesMap [(5,4),(2,1),(35,7)] == [(9,40,41),(3,4,5),(1176,490,1274)])
+
+testPythTriplesMapB: Bool
+testPythTriplesMapB = (pythTriplesMap [(7,8), (9,7)] == [(-15,112,113),(32,126,130)])
+
+
+testPythTriplesRec: Bool
+testPythTriplesRec = (pythTriplesRec [(5,4),(2,1),(35,7)] == [(9,40,41),(3,4,5),(1176,490,1274)])
+
+
+testPythTriplesRecB: Bool
+testPythTriplesRecB = (pythTriplesRec [(6,4),(2,1),(20,7)] == [(20,48,52),(3,4,5),(351,280,449)]) 
+
+
+testIsTripleZeros: Bool
+testIsTripleZeros =  (isTriple 0 0 0 == False)
+
+testIsTripleNotEqual: Bool
+testIsTripleNotEqual = (isTriple 6 5 4 == False)
+
+testIsTripleNotEqual2: Bool
+testIsTripleNotEqual2 = (isTriple 4 6 5 == False)
+
+testIsTripleEqual: Bool
+testIsTripleEqual =(isTriple 3 4 5 == True)
+
+
+testPythTriple: Bool
+testPythTriple = (pythTriple (4, 5) == (-9, 40, 41))
+
+testPythTripleB: Bool
+testPythTripleB = (pythTriple (7, 3) == (40, 42, 58))
+
+testisTripleTuple: Bool
+testisTripleTuple = (isTripleTuple (3, 4, 5) == True)
+
+
+testArePythTriplesFilter: Bool
+testArePythTriplesFilter = (arePythTriplesFilter [(1,2,3), (9,40,41), (3,4,5), (100,2,500)]   == [(9,40,41),(3,4,5)] )
+
+
+testArePythTriplesFilterB: Bool
+testArePythTriplesFilterB = (arePythTriplesFilter [(1,2,3), (9,40,41), (3,4,5), (360,2,500)] == [(9,40,41),(3,4,5)] )
+
+
+testArePythTriplesRec: Bool
+testArePythTriplesRec = (arePythTriplesRec [(1,2,3), (9,40,41), (3,4,5), (100,2,500)]   == [(9,40,41),(3,4,5)] )
+
+testArePythTriplesRecB: Bool
+testArePythTriplesRecB = (arePythTriplesRec [(1,2,3), (9,40,41), (3,4,5), (360,2,500)] == [(9,40,41),(3,4,5)] )
+
+
+allTests = [testPythTriplesMap, testPythTriplesMapB, testPythTriplesRec, testPythTriplesRecB, testIsTripleZeros, testIsTripleNotEqual, testIsTripleNotEqual2, testIsTripleEqual, testPythTriple, testPythTripleB, testisTripleTuple, testArePythTriplesFilter, testArePythTriplesFilterB, testArePythTriplesRec, testArePythTriplesRecB]
